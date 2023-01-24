1c34a23c973a69dda126517ce6b1aa0a45f9d565
bmpschneider <brunopschneider@gmail.com>
2023-01-23 22:07:14 -0300

    first commit

---
+++ components/atoms/Logo.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div class="logo">
+      <img src="@/assets/img/logo.svg" alt="iBook" />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.logo {
+  width: 100%;
+  img {
+    width: 100%;
+  }
+}
+</style>
